b5e2bf9dd57bbf10025a835679ec3a56bcc2c05e
Jeff Wenzbauer <jwenz723@gmail.com>
2021-04-30 20:06:40 -0600

    Improve Stability (#1)
    * added comment
    
    * getting test workflow to work
    
    * update documentation
    
    * fix lint issues
    
    * fix lint issues
    
    * revert test.yaml workflow
    
    * added unit test
    
    * prettify
    
    * bump version

--- src/index.ts
+++ src/index.ts
@@ -1,62 +1,38 @@
 import { getInput, info, setFailed, setOutput, setSecret } from "@actions/core";
 import isBase64 from "is-base64";
-import { createAppAuth } from "@octokit/auth-app";
-
-const fetchInstallationToken = async ({
-     appId,
-     clientId,
-     clientSecret,
-     installationId,
-     privateKey,
- }: Readonly<{
-    appId: string;
-    clientId: string;
-    clientSecret: string;
-    installationId: string;
-    privateKey: string;
-}>): Promise<string> => {
-    const auth = createAppAuth({
-        appId: appId,
-        privateKey: privateKey,
-        clientId: clientId,
-        clientSecret: clientSecret,
-    });
-    const installation = await auth({
-        type: "installation",
-        installationId: installationId,
-    });
-    return installation.token
-};
+import { fetchInstallationToken } from "./fetch-installation-token";
 
 const run = async () => {
-    try {
-        const appId = getInput("app_id", { required: true });
-        const clientId = getInput("client_id", { required: true });
-        const clientSecret = getInput("client_secret", { required: true });
-        const installationId = getInput("installation_id", { required: true });
-        const privateKeyInput = getInput("private_key", { required: true });
-        const privateKey = isBase64(privateKeyInput)
-            ? Buffer.from(privateKeyInput, "base64").toString("utf8")
-            : privateKeyInput;
+  try {
+    const appId = getInput("app_id", { required: true });
+    const clientId = getInput("client_id", { required: true });
+    const clientSecret = getInput("client_secret", { required: true });
+    const installationId = getInput("installation_id", { required: true });
+    const privateKeyInput = getInput("private_key", { required: true });
+    const privateKey = isBase64(privateKeyInput)
+      ? Buffer.from(privateKeyInput, "base64").toString("utf8")
+      : privateKeyInput;
 
-        const installationToken = await fetchInstallationToken({
-            appId,
-            clientId,
-            clientSecret,
-            installationId,
-            privateKey
-        })
+    const installationToken = await fetchInstallationToken(
+      {
+        appId: appId,
+        clientId: clientId,
+        clientSecret: clientSecret,
+        privateKey: privateKey,
+      },
+      installationId,
+    );
 
-        setSecret(installationToken);
-        setOutput("token", installationToken);
-        info("Token generated successfully!");
-    } catch (error: unknown) {
-        if (typeof error === "string" || error instanceof Error) {
-            setFailed(error);
-        } else {
-            setFailed(`Caught error of unexpected type: ${typeof error}`);
-        }
+    setSecret(installationToken);
+    setOutput("token", installationToken);
+    info("Token generated successfully!");
+  } catch (error: unknown) {
+    if (typeof error === "string" || error instanceof Error) {
+      setFailed(error);
+    } else {
+      setFailed(`Caught error of unexpected type: ${typeof error}`);
     }
+  }
 };
 
 void run();
